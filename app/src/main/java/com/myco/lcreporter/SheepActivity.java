794acc982cec2430b12e39fce24d5c6429624d07
Andre Filliettaz <andrentaz@gmail.com>
2015-02-12 00:54:39 -0200

    Cansei de Fragments e vou fazer ele abrir outra Activity

--- app/src/main/java/com/myco/lcreporter/SheepActivity.java
+++ app/src/main/java/com/myco/lcreporter/SheepActivity.java
@@ -12,22 +12,5 @@ public class SheepActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sheep);
-
-        // Check that the activity is using the layout version with the
-        // fragment_container FrameLayout
-        if (findViewById(R.id.contacts_list_container) != null) {
-            // In case it is being restored
-            if (savedInstanceState != null)
-                return;
-        }
-
-        ContactsFragment fragment = new ContactsFragment();
-
-        // Pass arguments to the fragment
-        fragment.setArguments(getIntent().getExtras());
-
-        // Add the fragment to the container FrameLayout
-        getFragmentManager().beginTransaction()
-                .add(R.id.contacts_list_container, fragment).commit();
     }
 }
